797145c333e9a5ef6bb7604c329952073892a8c6
Stefano Armenes <stefano.armenes@gmail.com>
2023-03-15 11:33:06 +0100

    feat(csv): csv upload (#19)

--- packages/crawler/README.md
+++ packages/crawler/README.md
@@ -10,6 +10,8 @@ Create a new notion integration and add it to a section. Please refer to https:/
 
 Head to https://www.notion.so/my-integrations/ and select your. Then copy/paste the following values in an `.env` file:
 
-| Env var        |                                |
-| -------------- | ------------------------------ |
-| `NOTION_TOKEN` | secret_y0uR_1n73gr4710n_S3cr37 |
+| Env var                  |                                |
+| ------------------------ | ------------------------------ |
+| `NOTION_TOKEN`           | secret_y0uR_1n73gr4710n_S3cr37 |
+| `GC_STORAGE_BUCKET_NAME` | your_bucket_name               |
+| `FILE_NAME`              | fallback on "notion.csv"       |
---
+++ packages/crawler/src/file-upload.ts
@@ -0,0 +1,27 @@
+import stream from 'stream'
+import { Storage } from '@google-cloud/storage'
+
+export const upload = (csv: string) => {
+  const bucketName = process.env.GC_STORAGE_BUCKET_NAME || ''
+  const destFileName = process.env.FILE_NAME || 'notion.csv'
+
+  const storage = new Storage()
+
+  const myBucket = storage.bucket(bucketName)
+  const file = myBucket.file(destFileName)
+
+  const passthroughStream = new stream.PassThrough()
+  passthroughStream.write(csv)
+  passthroughStream.end()
+
+  async function streamFileUpload() {
+    passthroughStream
+      .pipe(file.createWriteStream())
+      .on('finish', () => {
+        console.log('Upload completed!')
+      })
+      .on('error', e => console.error('Upload failed with error: ', e))
+  }
+
+  streamFileUpload().catch(console.error)
+}
