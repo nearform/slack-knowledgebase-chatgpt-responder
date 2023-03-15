852c01c0b4f638d85782e880bd931f3d02d0e975
Stefano Armenes <stefano.armenes@gmail.com>
2023-03-15 11:32:29 +0100

    feat(csv): csv upload

--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+import * as ff from '@google-cloud/functions-framework'
 import { generateCsv } from './csv'
 import { fetchData } from './notion'
+import { upload } from './file-upload'
 
-fetchData().then(generateCsv)
+ff.http('crawl', (req, res) => {
+  fetchData()
+    .then(generateCsv)
+    .then(csv => {
+      upload(csv)
+      res.send('Upload started!')
+    })
+})
