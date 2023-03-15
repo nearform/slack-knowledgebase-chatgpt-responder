991b9744b560702a9d5b95aa2fa9c00eb8312438
Stefano Armenes <stefano.armenes@gmail.com>
2023-03-15 12:14:08 +0100

    fix(csv): renamed header from content to text

--- packages/crawler/src/csv.ts
+++ packages/crawler/src/csv.ts
@@ -3,7 +3,7 @@ import json2csv from 'json-2-csv'
 export const generateCsv = async (
   data: {
     title: string | null
-    content: string
+    text: string
   }[]
 ) => {
   return json2csv.json2csvAsync(data)
