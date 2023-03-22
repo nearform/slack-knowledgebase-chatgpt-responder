924f720d02c887b3fc77ec0db94bbbd9c70a1f75
Stefano Armenes <stefano.armenes@gmail.com>
2023-03-22 11:11:47 +0100

    chore: testing the notion interface function (#62)
    * chore: testing the notion interface function
    
    * perf: fix notion.js test, added cross-env
    
    * perf: move json mocks inside a mocks folder

---
+++ packages/crawler/test/notion.js
@@ -0,0 +1,29 @@
+import tap from 'tap'
+import esmock from 'esmock'
+
+import fakePageResponse from './mocks/page-response.json' assert { type: 'json' }
+import fakeChildrenResponse from './mocks/children-response.json' assert { type: 'json' }
+
+class NotionClientMock {
+  constructor() {}
+
+  async search() {
+    return fakePageResponse
+  }
+
+  blocks = {
+    children: {
+      list: async ({ block_id }) => fakeChildrenResponse[block_id]
+    }
+  }
+}
+
+tap.test('fetchData returns correct parsed data', async t => {
+  const { fetchData } = await esmock('../src/notion.js', {
+    '@notionhq/client': {
+      Client: NotionClientMock
+    }
+  })
+
+  t.matchSnapshot(await fetchData(), 'fetchData() result does not match!')
+})
