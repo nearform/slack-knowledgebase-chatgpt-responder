0525e9deda69e81d30ad7670bbbc396a403bf5a2
Stefano Armenes <stefano.armenes@gmail.com>
2023-03-23 16:15:23 +0100

    test: add tests to cover csv.js and index.js

---
+++ packages/crawler/tap-snapshots/test/csv.js.test.cjs
@@ -0,0 +1,12 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/csv.js TAP csv2json works correctly > must match snapshot 1`] = `
+index,title,text
+0,Super blog post,This is awesome!
+1,Testing with snapshots,It is super simple!
+`
---
+++ packages/crawler/test/index.js
@@ -0,0 +1,61 @@
+import tap from 'tap'
+import esmock from 'esmock'
+import sinon from 'sinon'
+
+tap.test('successfull', async t => {
+  const fetchDataMock = sinon.fake.returns(
+    Promise.resolve([
+      { index: 0, title: 'Super blog post', text: 'This is awesome!' }
+    ])
+  )
+  const uploadMock = sinon.fake()
+
+  const { crawl, CMD } = await esmock('../src/index.js', {
+    '@google-cloud/functions-framework': {
+      ff: (_, cb) => {
+        cb()
+      }
+    },
+    '../src/notion.js': {
+      fetchData: fetchDataMock
+    },
+    '../src/file-upload.js': {
+      upload: uploadMock
+    }
+  })
+
+  await crawl(CMD)
+  t.equal(uploadMock.callCount, 1)
+  t.ok(
+    uploadMock.calledWith(
+      'index,title,text\n0,Super blog post,This is awesome!'
+    )
+  )
+  t.equal(fetchDataMock.callCount, 1)
+})
+
+tap.test('wrong command', async t => {
+  const fetchDataMock = sinon.fake.returns(Promise.resolve([]))
+  const uploadMock = sinon.fake()
+
+  sinon.spy(console, 'log')
+
+  const { crawl } = await esmock('../src/index.js', {
+    '@google-cloud/functions-framework': {
+      ff: (_, cb) => {
+        cb()
+      }
+    },
+    '../src/notion.js': {
+      fetchData: fetchDataMock
+    },
+    '../src/file-upload.js': {
+      upload: uploadMock
+    }
+  })
+
+  await crawl('wrong_cmd')
+  t.equal(uploadMock.callCount, 0)
+  t.equal(fetchDataMock.callCount, 0)
+  t.ok(console.log.calledWith('noop, received "wrong_cmd" command'))
+})
