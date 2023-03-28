27f720f9cec8dac867a0063f9c2484e8da7ec7d4
Stefano Armenes <stefano.armenes@gmail.com>
2023-03-28 15:22:37 +0200

    feat: paginate search request to optimize the result (#104)
    * feat: paginate search request to optimize the result
    
    * test: refactor notion test, wip
    
    * test: added getPages test

--- packages/crawler/src/notion.js
+++ packages/crawler/src/notion.js
@@ -12,27 +12,43 @@ const getPageTitle = (item, fieldName) => {
     const field = item.properties[fieldName]
 
     if (field && 'title' in field) {
-      return 'title' in field ? field.title[0].plain_text : null
+      return 'title' in field ? field.title[0]?.plain_text : null
     }
   }
 
   return null
 }
 
-const getPages = async () => {
-  const result = await notion.search({
-    filter: {
-      value: 'page',
-      property: 'object'
-    }
-  })
+export const getPages = async () => {
+  let data = []
+  let hasMore = true
+  let cursor = undefined
 
-  return result.results.map(item => {
-    const id = item.id
-    const title = getPageTitle(item, 'Name') || getPageTitle(item, 'title')
+  while (hasMore) {
+    const result = await notion.search({
+      filter: {
+        value: 'page',
+        property: 'object'
+      },
+      start_cursor: cursor,
+      page_size: 10
+    })
 
-    return { id, title }
-  })
+    hasMore = result.has_more
+    cursor = result.next_cursor
+
+    data = [
+      ...data,
+      ...result.results.map(item => {
+        const id = item.id
+        const title = getPageTitle(item, 'Name') || getPageTitle(item, 'title')
+
+        return { id, title }
+      })
+    ]
+  }
+
+  return data
 }
 
 const getPageContent = async id => {
