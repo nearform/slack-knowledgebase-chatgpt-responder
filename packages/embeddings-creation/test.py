4274e1afa41212f2435c5bd00c03c4e439b62fba
Andrea Carraro <andrea.carraro@nearform.com>
2023-03-29 16:39:06 +0200

    test(embedding-creation): fix regression error

--- packages/embeddings-creation/test.py
+++ packages/embeddings-creation/test.py
@@ -1,4 +1,3 @@
-from google.cloud import storage
 import unittest
 import main
 import openai
@@ -37,36 +36,30 @@ openAIEmbeddingsResponseMock = {
 }
 
 
-def createScrapedFile(_):
+def createScrapedFile(_, __, ___):
     with open(scraped_file_name, "w") as f:
         f.write(scrapedFileMock)
 
 
 class TestEmbeddingsCreation(unittest.TestCase):
-    @patch("google.cloud.storage.Client")
+    @patch('main.download_from_bucket_to_path')
     @mock.patch.dict(
         "os.environ",
         {"GCP_STORAGE_SCRAPED_FILE_NAME": scraped_file_name, "GCP_STORAGE_EMBEDDING_FILE_NAME": embeddings_file_name},
         clear=True,
     )
-    def test_embeddings_creations(self, client_mock):
+    def test_embeddings_creations(self, download_from_bucket_to_path_mock):
         openai.Embedding.create = MagicMock(return_value=openAIEmbeddingsResponseMock)
 
-        # Create a mock bucket and blob
-        bucket_mock = MagicMock()
-        blob_mock = MagicMock()
-        blob_mock.download_to_filename.side_effect = createScrapedFile
-
-        client_mock.return_value.bucket.return_value = bucket_mock
-        bucket_mock.blob.return_value = blob_mock
+        download_from_bucket_to_path_mock.side_effect = createScrapedFile
 
         cloud_event = CloudEventMock()
         main.create_embeddings(cloud_event)
-
-        generatedEmbeddingsFile = open(embeddings_file_name, "r")
-        actualEmbeddings = generatedEmbeddingsFile.read()
-        expectedEmbeddings = ",text,n_tokens,embeddings\n0,Page content,2,[-0.010027382522821426]\n"
-        self.assertEqual(actualEmbeddings, expectedEmbeddings)
+        
+        with open(embeddings_file_name, 'r') as f:
+          actualEmbeddings = f.read()
+          expectedEmbeddings = ",text,n_tokens,embeddings\n0,Page content,2,[-0.010027382522821426]\n"
+          self.assertEqual(actualEmbeddings, expectedEmbeddings)
 
 
 if __name__ == "__main__":
