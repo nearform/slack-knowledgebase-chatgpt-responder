d3b0f6b40cf8d47d562dc546c2fc0828c0e67da7
Stefano Armenes <stefano.armenes@gmail.com>
2023-04-03 16:29:42 +0200

    feat: crawl, recursive traverse the block tree (#120)
    * feat: crawl, recursive traverse the block tree, removed function-framework to use cloud job
    
    * perf: crawl, avoid call to block children if not needed
    
    * docs: updated crawl section
    
    * docs: fix cron job creation
    
    * chore: updated deploy-step
    
    * chore: updated deploy-step, removing tag
    
    * fix: fix husky install
    
    * chore: quiet on gcloud builds image
    
    * chore: --suppress-logs flag
    
    * chore: added beta component in cloud sdk setup
    
    * chore: added $GITHUB_SHA
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix crawler img path
    
    * fix: filter away pages without content
    
    * test: update snapshot
    
    * perf: promisified setTimeout

--- packages/crawler/src/index.js
+++ packages/crawler/src/index.js
@@ -1,21 +1,3 @@
-import 'dotenv/config'
+import { crawl } from '../src/crawl.js'
 
-import * as ff from '@google-cloud/functions-framework'
-import { generateCsv } from './csv.js'
-import { fetchData } from './notion.js'
-import { upload } from './file-upload.js'
-
-export const CMD = 'start_crawl'
-
-export const crawl = async cmd => {
-  if (cmd === CMD) {
-    await fetchData().then(generateCsv).then(upload)
-  } else {
-    console.log(`noop, received "${cmd}" command`)
-  }
-}
-
-ff.cloudEvent('crawl', cloudEvent => {
-  const cmd = Buffer.from(cloudEvent.data.message.data, 'base64').toString()
-  crawl(cmd)
-})
+crawl()
