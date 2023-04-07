c6b2c5eaf2a0a456f2b55b4083831d1a762c9ae9
Stefano Armenes <stefano.armenes@gmail.com>
2023-04-07 16:00:42 +0200

    fix: added missing min instance on slack-bot deploy, fix also getAnswer maxTokens (#147)
    * fix: added missing min instance on slack-bot deploy, fix also getAnswer maxTokens
    
    * fix: tokens to 300
    
    * fix: getAnswer.test

--- packages/slack-bot/src/getAnswer.js
+++ packages/slack-bot/src/getAnswer.js
@@ -142,7 +142,7 @@ async function getAnswer({
   question = 'What is NearForm?',
   maxLength = 1800,
   embeddingModel = defaultEmbeddingModel,
-  maxTokens = 150,
+  maxTokens = 300,
   stopSequence
 }) {
   await initializationPromise
