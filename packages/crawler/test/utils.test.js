3ea5901e5b4f36b02c28b3fca219b65d4af165e8
Simone Busoli <simone.busoli@nearform.com>
2025-04-13 21:10:58 +0200

    test: node test migration (#626)

--- packages/crawler/test/utils.test.js
+++ packages/crawler/test/utils.test.js
@@ -1,7 +1,7 @@
-import tap from 'tap'
+import { test } from 'node:test'
 import { createCsv } from '../src/utils.js'
 
-tap.test('createCsv works correctly', async t => {
+test('createCsv works correctly', async t => {
   const actual = await createCsv([
     { index: 0, title: 'Super blog post', text: 'This is awesome!' },
     { index: 1, title: 'Testing with snapshots', text: 'It is super simple!' }
@@ -11,5 +11,5 @@ tap.test('createCsv works correctly', async t => {
 0,Super blog post,This is awesome!
 1,Testing with snapshots,It is super simple!`
 
-  t.equal(actual, expected)
+  t.assert.strictEqual(actual, expected)
 })
